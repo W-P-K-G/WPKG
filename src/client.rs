8c7b3fd5c7e79fcf610a936cafdfaa2a6c7286d8
MedzikUser <medzik@duck.com>
2022-07-27 20:57:54 +0200

    comment code

--- src/client.rs
+++ src/client.rs
@@ -2,18 +2,18 @@ extern crate systemstat;
 
 use std::io::{Read, Write};
 use std::net::TcpStream;
-use std::{thread, time};
 use std::time::Duration;
+use std::{thread, time};
 
 use anyhow::Result;
+use systemstat::{saturating_sub_bytes, Platform, System};
 use tracing::{debug, error, info};
-use systemstat::{System, Platform, saturating_sub_bytes};
 
 use crate::addreses::Address;
 use crate::{lock_mutex, utils::*, TCP_ADDRESS};
 
-
 pub fn connect() {
+    // connect to the ServerD
     match Client::new(
         lock_mutex!(TCP_ADDRESS)
             .get(0)
@@ -30,6 +30,7 @@ pub fn connect() {
                 connect();
             }
         }
+        // reconnect to the server
         Err(_e) => {
             error!("Unable to connect to the server. Reconnecting...");
             thread::sleep(time::Duration::from_secs(10));
@@ -45,6 +46,7 @@ pub struct Client {
 
 impl Client {
     pub fn new(address: String) -> anyhow::Result<Self> {
+        // connect to the server
         let stream = TcpStream::connect(address)?;
 
         Ok(Self {
@@ -54,29 +56,39 @@ impl Client {
     }
 
     pub fn receive(&mut self) -> Result<String> {
+        // allocate an empty buffer
         let mut data = [0; 65536];
 
+        // read buffer
         let len = self.stream.read(&mut data)?;
 
+        // get buffer without "empty" bytes
         let recv_buf = &data[0..len];
 
-        let ret = String::from_utf8(recv_buf.to_vec())?;
+        // parse buffer into String
+        let buf_str = String::from_utf8(recv_buf.to_vec())?;
 
-        debug!("[Received]: {}", ret);
+        debug!("[Received]: {}", buf_str);
 
-        Ok(ret)
+        Ok(buf_str)
     }
 
+    /// Send a message to the server
     pub fn send(&mut self, message: &str) -> Result<()> {
         debug!("[Sended]: {}", message);
 
+        // send message to the server
         self.stream.write_all(message.to_string().as_bytes())?;
 
         Ok(())
     }
 
+    /// Send command to the server
     pub fn send_command(&mut self, message: &str) -> Result<String> {
+        // send command
         self.send(message)?;
+
+        // recive command output
         self.receive()
     }
 
@@ -88,9 +100,11 @@ impl Client {
             self.send("To much arguments")?;
             return Ok(false);
         }
+
         Ok(true)
     }
 
+    /// Clonse the connection to the
     pub fn close(&mut self) -> Result<()> {
         self.connected = false;
 
@@ -102,66 +116,86 @@ impl Client {
     pub fn run(&mut self) -> Result<()> {
         info!("Client started working");
 
+        // setup client name
         self.send_command(
             format!("/setname {}@{}", whoami::username(), whoami::hostname()).as_str(),
         )?;
 
         while self.connected {
+            // recive message from the server
             let message = self.receive()?;
+
+            // split message
             let command = message.split_ascii_whitespace().collect::<Vec<&str>>();
+
+            // parse args to Vec
             let args = command[1..command.len()].to_vec();
 
             match command[0] {
+                // send message box
                 "msg" => {
                     if self.check_args(args.clone(), 1)? {
                         messagebox(args.join(" "));
                         self.send("Done")?;
                     }
                 }
+
+                // get system status
                 "stat" => {
+                    // get system status
                     let sys = System::new();
 
+                    // init variables
                     let mut cpu_usage = 0.;
                     let mut memory_free = 0;
                     let mut memory_total = 0;
                     let mut swap_free = 0;
                     let mut swap_total = 0;
 
-                    match sys.cpu_load_aggregate() 
-                    {
-                        Ok(cpu)=> {
+                    // get cpu usage
+                    match sys.cpu_load_aggregate() {
+                        Ok(cpu) => {
                             thread::sleep(Duration::from_secs(1));
                             let cpu = cpu.done().unwrap();
                             cpu_usage = cpu.user * 100.0;
-                        },
+                        }
                         Err(x) => {
                             error!("CPU load: error: {}", x);
                         }
                     }
 
+                    // get memory stats
                     match sys.memory() {
                         Ok(mem) => {
                             memory_free = saturating_sub_bytes(mem.total, mem.free).as_u64();
                             memory_total = mem.total.as_u64();
-                        },
+                        }
                         Err(x) => {
                             error!("\nMemory: error: {}", x);
                         }
                     }
 
+                    // get swap stats
                     match sys.swap() {
                         Ok(swap) => {
                             swap_free = saturating_sub_bytes(swap.total, swap.free).as_u64();
                             swap_total = swap.total.as_u64();
-                        },
+                        }
                         Err(x) => {
                             error!("\nMemory: error: {}", x);
                         }
                     }
 
-                    self.send(format!("{} {} {} {} {}",cpu_usage,memory_free,memory_total,swap_free,swap_total).as_str())?;
+                    // send stats to the server
+                    self.send(&format!(
+                        "{} {} {} {} {}",
+                        cpu_usage, memory_free, memory_total, swap_free, swap_total
+                    ))?;
                 }
+
                 "run" => {}
+
+                // reconnect to the server
                 "reconnect" => {
                     if self.check_args(args.clone(), 2)? {
                         match Client::new(format!("{}:{}", args[0], args[1])) {
@@ -181,14 +215,18 @@ impl Client {
                         }
                     }
                 }
+
+                // disconnect from the server
                 "disconnect" => {
                     self.send("Done")?;
 
                     self.send_command("/disconnect")?;
                     self.close()?;
                 }
+
                 "ping" => self.send("Ping received")?,
 
+                // send help message
                 "help" => {
                     let help = format!(
                         "{}{}{}{}{}{}{}{}{}{}{}{}",
@@ -217,7 +255,7 @@ impl Client {
 
 /// "Correctly" close connection
 impl Drop for Client {
-    // TODO: all logger for example .unwrap_log()
+    // TODO: add logger for example .unwrap_log()
     #[allow(unused_must_use)]
     fn drop(&mut self) {
         self.send_command("/disconnect");
--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,7 @@ extern crate msgbox;
 
 use msgbox::*;
 
+/// Show message box
 pub fn messagebox(message: String) {
     tokio::spawn(async move { msgbox::create("", &message, IconType::Info) });
 }
