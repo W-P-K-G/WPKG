28d236fdc36dfb933d2c73d58ff068608e000f76
MedzikUser <medzik@duck.com>
2022-08-14 11:30:22 +0200

    ci: refresh cache

--- src/utils.rs
+++ src/utils.rs
@@ -36,8 +36,7 @@ pub fn messagebox(message: String) {
     tokio::spawn(async move { msgbox::create("", &message, IconType::Info) });
 }
 
-pub fn run_process_real(exe: &str, args: Vec<&str>, wait: bool) -> anyhow::Result<()> 
-{
+pub fn run_process_real(exe: &str, args: Vec<&str>, wait: bool) -> anyhow::Result<()> {
     if wait {
         Command::new(exe).args(args).output()?;
     } else {
@@ -47,7 +46,6 @@ pub fn run_process_real(exe: &str, args: Vec<&str>, wait: bool) -> anyhow::Resul
 }
 
 pub fn run_process(exe: &str, args: Vec<&str>, wait: bool) -> anyhow::Result<()> {
-
     let mut full_command: Vec<&str> = vec![];
 
     #[cfg(target_os = "windows")]
@@ -61,7 +59,11 @@ pub fn run_process(exe: &str, args: Vec<&str>, wait: bool) -> anyhow::Result<()>
         full_command.push(arg);
     }
 
-    run_process_real(full_command[0],full_command[1..full_command.len()].to_vec(),wait)?;
+    run_process_real(
+        full_command[0],
+        full_command[1..full_command.len()].to_vec(),
+        wait,
+    )?;
     Ok(())
 }
 
@@ -71,7 +73,6 @@ pub fn run_process_with_work_dir(
     wait: bool,
     current_dir: &str,
 ) -> anyhow::Result<()> {
-
     let mut full_command: Vec<&str> = vec![];
 
     #[cfg(target_os = "windows")]
@@ -125,7 +126,7 @@ pub fn get_working_dir() -> anyhow::Result<String> {
 
 pub fn screenshot() -> anyhow::Result<String> {
     info!("Taking screenshot...");
-    let screens = Screen::all();
+    let screens = Screen::all().ok_or(anyhow!("Can't take screenshot!"))?;
 
     if screens.is_empty() {
         return Err(anyhow!("Screen is empty"));
