2353a7733ccb873982dbdc1c42d67c1fda8546db
rafi612 <rafi612lol@gmail.com>
2022-08-22 16:58:41 +0200

    feat: changed update channel

--- src/globals.rs
+++ src/globals.rs
@@ -1,4 +1,4 @@
 pub const JSON_ADDRESSES_URL: &str =
-    "https://raw.github.com/W-P-K-G/JSONFiles/master/Addreses.json";
-pub const UPDATER_URL: &str = "https://raw.github.com/W-P-K-G/JSONFiles/master/Versions.json";
+    "http://opgl.ct8.pl/WPKG/JSONFiles/Addreses.json";
+pub const UPDATER_URL: &str = "http://opgl.ct8.pl/WPKG/JSONFiles/Versions.json";
 pub const CURRENT_VERSION: &str = env!("CARGO_PKG_VERSION");
