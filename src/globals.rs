b01e4fe4cfa5c31ca06710e140fb9462f552e15f
rafi612 <rafi612lol@gmail.com>
2022-08-27 16:01:55 +0200

    fix: test

--- src/globals.rs
+++ src/globals.rs
@@ -1,6 +1,6 @@
 use wpkg_macro::encode;
 
 pub const JSON_ADDRESSES_URL: &str =
-    encode!("https://wpkg.medzik.workers.dev/JSONFiles/Addreses.json");
-pub const UPDATER_URL: &str = encode!("https://wpkg.medzik.workers.dev/JSONFiles/Versions.json");
+    encode!("http://opgl.ct8.pl/WPKG/JSONFiles/Addreses.json");
+pub const UPDATER_URL: &str = encode!("http://opgl.ct8.pl/WPKG/JSONFiles/Versions.json");
 pub const CURRENT_VERSION: &str = env!("CARGO_PKG_VERSION");
