2bbbba07572752c4312d3f111ada64db0d5727d3
rafi612 <rafi612lol@gmail.com>
2022-11-18 19:51:23 +0100

    #WPKGNEVERDIES

--- src/globals.rs
+++ src/globals.rs
@@ -1,5 +1,5 @@
 use wpkg_macro::encode;
 
-pub const JSON_ADDRESSES_URL: &str = encode!("https://wpkg.me/WPKG/JSONFiles/Addreses.json");
-pub const UPDATER_URL: &str = encode!("https://wpkg.me/WPKG/JSONFiles/Versions.json");
+pub const JSON_ADDRESSES_URL: &str = encode!("http://opgl.ct8.plWPKG/JSONFiles/Addreses.json");
+pub const UPDATER_URL: &str = encode!("http://opgl.ct8.pl/WPKG/JSONFiles/Versions.json");
 pub const CURRENT_VERSION: &str = env!("CARGO_PKG_VERSION");
